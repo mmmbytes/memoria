610379e6ecd6caae1650c5c831f483962b08be9e
Mia <mia001.tapia@gmail.com>
2022-11-13 16:34:40 -0800

    Add React framework

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Memoria', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appHeader = screen.getByText("Memoria");
+  expect(appHeader).toBeInTheDocument();
 });
