f025aab0506109abe8752c4d440f7f09c53b2ccd
Mia Tapia <114131830+mmmbytes@users.noreply.github.com>
2022-12-30 17:22:50 -0800

    Add icons to navigation bar.  (#12)

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { VscClose } from "react-icons/vsc";
+import { VscMenu } from "react-icons/vsc";
 import './NavBar.css'
 
 const NavBar = () => {
@@ -9,15 +11,20 @@ const NavBar = () => {
     }
 
     return (
-    <nav className="nav-bar">
-      <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
-      <ul className={`nav-menu ${navbarOpen ? "show-menu " : ""}`}>
-        <div className="nav-links">
-          <li>Contents</li>
-          <li>Discover</li>
+      <nav className="nav-bar">
+        <div className="nav-flex-container">
+          <button className="menu-icon" onClick={handleToggle}>
+            {navbarOpen ? <VscClose /> : <VscMenu />}
+          </button>
+          <ul className={`nav-menu ${navbarOpen ? "show-menu " : ""}`}>
+            <div className="nav-links">
+              <li>Contents</li>
+              <li>Discover</li>
+            </div>
+          </ul>
+          <button className="home-icon">Memoria</button>
         </div>
-      </ul>
-    </nav>
+      </nav>
     );
 }
 
