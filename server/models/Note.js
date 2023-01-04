94ef54679d50130d9a31eaf46092d3af4d017132
Mia Tapia <114131830+mmmbytes@users.noreply.github.com>
2023-01-03 23:18:47 -0800

    Create the note database schema. (#14)

---
+++ server/models/Note.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+    title: String,
+    text: String,
+    images: String,
+    date: { type: Date, default: Date.now },
+    tags: [String],
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
