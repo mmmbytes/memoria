66dfebefcb4f964013e6c179c57b106fa3c30281
Mia Tapia <114131830+mmmbytes@users.noreply.github.com>
2023-01-19 00:27:47 -0800

    Add the initial router implementation.  (#17)
    * Add initial router implementations.
    
    * Remove unnecessary files.

---
+++ server/routes/notes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+// GET all notes
+router.get('/', () => {
+    res.json({ message: 'GET all notes' });
+});
+
+// GET a single note
+router.get('/:id', (req, res) => {
+    res.json({ message: 'GET a single note' });
+});
+
+// POST a new note
+router.post('/', (req, res) => {
+    res.json({ message: 'POST a new note' });
+});
+
+// DELETE a note
+router.delete('/:id', (req, res) => {
+    res.json({ message: 'DELETE a note' });
+});
+
+// UPDATE a note
+router.patch('/:id', (req, res) => {
+    res.json({ message: 'UPDATE a note' });
+});
+
+module.exports = router;
