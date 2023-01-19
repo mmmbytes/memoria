2aa34a7166490a3251653fb38a78bafb2aadbb38
Mia <mia001.tapia@gmail.com>
2023-01-19 00:04:18 -0800

    Resolve merge conflicts

--- client/src/Main.js
+++ client/src/Main.js
@@ -5,11 +5,7 @@ function Main() {
         <div className="app-main">
             <div className="note-edit">
                 <input type="text" id="note-title" placeholder="Title" autoFocus />
-<<<<<<< HEAD:client/src/Main.js
                 <textarea id="note-body" placeholder="Begin your writing journey here..."></textarea>
-=======
-                <textarea id="note-body" placeholder="Begin you writing journey here..."></textarea>
->>>>>>> main:src/Main.js
             </div>
         </div>
     ); 
