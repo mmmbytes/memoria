55455f36bdf1443135ae894e375c25147f6566eb
Mia <mia001.tapia@gmail.com>
2023-02-17 22:31:44 -0800

    Update the post function and the note model.

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const Note = require('../models/Note');
+const {
+    createNote,
+} = require('./../controllers/note.ctrl');
+
 
 const router = express.Router();
 
@@ -14,16 +17,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new note
-router.post('/', async (req, res) => {
-    const { title, text } = req.body;
-
-try {
-    const newNote = await Note.create(req.body);
-    res.status(200).json(newNote);
-} catch (error) {
-    res.status(400).json({ message: error.message });
-}
-});
+router.post('/', createNote);   
 
 // DELETE a note
 router.delete('/:id', (req, res) => {
