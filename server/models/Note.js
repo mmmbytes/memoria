4a71f38fdd60f5073967c85421827298960477d9
Mia <mia001.tapia@gmail.com>
2023-03-14 18:27:58 -0700

    Add a timestamp to the note schema.

--- server/models/Note.js
+++ server/models/Note.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
     title: String,
     textbody: String,
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Note', noteSchema);
