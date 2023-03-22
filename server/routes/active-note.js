ea0d5c23678d256f04e5d5882c463d297188aca2
Mia <mia001.tapia@gmail.com>
2023-03-21 22:52:44 -0700

    Consolidate all note routes into the note.js file.

--- server/routes/active-note.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getLatestNote } = require("../controllers/active-note");
-
-const router = express.Router();
-
-router.get("/", getLatestNote);
-
-module.exports = router;
