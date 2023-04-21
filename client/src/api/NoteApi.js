9c6729a3f88e7ca46d865e703854222e259f3a10
Mia <mia001.tapia@gmail.com>
2023-04-20 20:07:09 -0700

    Refactor NoteApi functions to use a common API request.

--- client/src/api/NoteApi.js
+++ client/src/api/NoteApi.js
@@ -1,64 +1,59 @@
-const fetchLatestNote = async () => {
+const apiRequest = async (url, options = {}) => {
 	try {
-		const response = await fetch("/api/notes/latest");
+		const response = await fetch(url, options);
 
 		if (response.status === 204) {
-			console.error("Notes collection is empty.");
+			console.error("Resource not found.");
 			return null;
 		}
 
-		const latestNote = await response.json();
-		return latestNote;
+		const responseData = await response.json();
+		return responseData;
 	} catch (error) {
-		console.error("Error loading note.");
+		console.error("Error in API request.");
 		return { error: error.message };
 	}
 };
 
+const fetchLatestNote = async () => {
+	const options = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	return apiRequest("/api/notes/latest", options);
+};
+
 const updateNote = async (noteId, updatedNote) => {
-	try {
-		const response = await fetch(`/api/notes/${noteId}`, {
-			method: "PUT",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(updatedNote),
-		});
-		const responseData = await response.json();
-		return responseData;
-	} catch (error) {
-		console.error("Error saving note.");
-		return null;
-	}
+	const options = {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(updatedNote),
+	};
+	return apiRequest(`/api/notes/${noteId}`, options);
 };
 
 const deleteNote = async (noteId) => {
-	try {
-		const response = await fetch(`/api/notes/${noteId}`, {
-			method: "DELETE",
-		});
-		const responseData = await response.json();
-		return responseData;
-	} catch (error) {
-		console.error("Error deleting note.");
-		return null;
-	}
+	const options = {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	return apiRequest(`/api/notes/${noteId}`, options);
 };
 
 const createNote = async () => {
-	try {
-		const response = await fetch("/api/notes", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-		const responseData = await response.json();
-		return responseData;
-	} catch (error) {
-		console.error("Error creating note.");
-		return null;
-	}
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	return apiRequest("/api/notes", options);
 };
 
 module.exports = {
