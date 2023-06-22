b2f4dac13e6cdcc07532c591c670cc8d899b7da7
Mia <mia001.tapia@gmail.com>
2023-06-22 14:39:40 -0700

    Update server by adding cors.

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ if (process.env.NODE_ENV !== 'UAT') {
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const noteRoutes = require('./routes/note');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
 	console.log(req.path, req.method);
