aff445b387b83988ab7ed31660597effe4ed4a84
Mia <mia001.tapia@gmail.com>
2023-06-23 12:02:52 -0700

    Add AuthContext to App root.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import { useState, FC } from 'react';
-// import AuthContext from './utils/AuthContext';
+import AuthContext from './utils/AuthContext';
 import AppRouter from './AppRouter';
 import './App.css';
 
 const App: FC = () => {
-	//const [isAuthenticated, setAuthStatus] = useState<boolean>(false);
+	const [isAuthenticated, setAuthStatus] = useState<boolean>(false);
 
 	return (
-		//	<AuthContext.Provider value={{ isAuthenticated, setAuthStatus }}>
-		<div className="App">
-			<AppRouter />
-		</div>
-		//	</AuthContext.Provider>
+		<AuthContext.Provider value={{ isAuthenticated, setAuthStatus }}>
+			<div className="App">
+				<AppRouter />
+			</div>
+		</AuthContext.Provider>
 	);
 };
 export default App;
