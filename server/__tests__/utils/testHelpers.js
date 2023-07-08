eb9f3851f393be119150ee1970d07b00809b88ea
Mia Tapia <114131830+mmmbytes@users.noreply.github.com>
2023-07-08 15:05:18 -0700

    Secure Cognito tokens using HttpOnly cookies. (#51)
    * Add cookie-parser dependency for parsing cookies in Express.
    * Sanitize authentication code to prevent injection attacks.
    * Implement HttpOnly cookies to enhance token security.
    * Increase nginx buffer size through a configuration file extension to handle larger payloads.
    * Refactor auth controller to provide more detailed JSON responses.
    * Update auth tests to validate correct configuration of HttpOnly cookies.
    * Introduce helper function to verify if cookies are defined and set as HttpOnly.

--- server/__tests__/utils/testHelpers.js
+++ server/__tests__/utils/testHelpers.js
@@ -10,4 +10,11 @@ function mockReqRes(params = {}, body = {}) {
 	return { req, res };
 }
 
-module.exports = { mockReqRes };
+function validateCookie(cookies, cookieName, cookieValue) {
+	const cookie = cookies.find((cookie) => cookie.startsWith(`${cookieName}=`));
+	expect(cookie).toBeDefined();
+	expect(cookie).toContain('HttpOnly');
+	expect(cookie).toContain(cookieValue);
+}
+
+module.exports = { mockReqRes, validateCookie };
