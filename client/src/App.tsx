7e6b1f906262546a77c75a5ea121d41c1f45cc26
Mia <mia001.tapia@gmail.com>
2023-07-26 13:06:16 -0700

    Refactor App.tsx to improve user authentication flow by checking for the presence of public 'isAuthenticated' cookie.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,51 @@
 import './App.css';
 
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
+import { useCookies } from 'react-cookie';
 
 import AppRouter from './AppRouter';
-import AuthContext, { User } from './utils/AuthContext';
+// import AuthContext, { User } from './utils/AuthContext';
 
 const App: FC = () => {
-	const [user, setUser] = useState<User>(null);
-	const [isLoading, setLoading] = useState<boolean>(true);
+	// const [user, setUser] = useState<User>(null);
+	// const [isLoading, setLoading] = useState<boolean>(true);
 
 	// Check if user's access and id cookies are set
-	useEffect(() => {
-		fetch('/api/auth/check', {
-			method: 'GET',
-			credentials: 'include',
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.isAuthenticated) {
-					setUser({ accessToken: true, idToken: true });
-				}
-				setLoading(false);
-			})
-			.catch((err) => {
-				console.error(err);
-				setLoading(false);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	fetch('/api/auth/check', {
+	// 		method: 'GET',
+	// 		credentials: 'include',
+	// 	})
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			if (data.isAuthenticated) {
+	// 				setUser({ accessToken: true, idToken: true });
+	// 			}
+	// 			setLoading(false);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.error(err);
+	// 			setLoading(false);
+	// 		});
+	// }, []);
 
-	// Show loading screen while checking authentication status
-	return isLoading ? (
-		<div>Loading...</div>
-	) : (
-		<AuthContext.Provider value={{ user, setUser }}>
-			<div className="App">
+	// // Show loading screen while checking authentication status
+	// return isLoading ? (
+	// 	<div>Loading...</div>
+	// ) : (
+	// 	<AuthContext.Provider value={{ user, setUser }}>
+
+	const [cookies] = useCookies(['isAuthenticated']);
+
+	return (
+		<div className="App">
+			{cookies.isAuthenticated ? (
 				<AppRouter />
-			</div>
-		</AuthContext.Provider>
+			) : (
+				(window.location.href = process.env.REACT_APP_LOGIN_URL)
+			)}
+		</div>
 	);
+	// </AuthContext.Provider>
 };
 export default App;
