402adb9097e27d9e922ccd8ee41f3b47dfbdfefa
Mia <mia001.tapia@gmail.com>
2023-07-26 21:57:07 -0700

    Final.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,46 +4,12 @@ import { FC, useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 
 import AppRouter from './AppRouter';
-// import AuthContext, { User } from './utils/AuthContext';
 
 const App: FC = () => {
-	// const [user, setUser] = useState<User>(null);
-	// const [isLoading, setLoading] = useState<boolean>(true);
-
-	// Check if user's access and id cookies are set
-	// useEffect(() => {
-	// 	fetch('/api/auth/check', {
-	// 		method: 'GET',
-	// 		credentials: 'include',
-	// 	})
-	// 		.then((res) => res.json())
-	// 		.then((data) => {
-	// 			if (data.isAuthenticated) {
-	// 				setUser({ accessToken: true, idToken: true });
-	// 			}
-	// 			setLoading(false);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.error(err);
-	// 			setLoading(false);
-	// 		});
-	// }, []);
-
-	// // Show loading screen while checking authentication status
-	// return isLoading ? (
-	// 	<div>Loading...</div>
-	// ) : (
-	// 	<AuthContext.Provider value={{ user, setUser }}>
-
 	const [cookies] = useCookies(['isAuthenticated']);
 	const [isLoading, setLoading] = useState<boolean>(true);
 
-	console.log(cookies.isAuthenticated);
-	console.log(cookies);
-
 	useEffect(() => {
-		console.log('This user is authenticated:');
-		console.log(cookies.isAuthenticated);
 		if (!cookies.isAuthenticated) {
 			window.location.href = process.env.REACT_APP_LOGIN_URL;
 		}
@@ -52,7 +18,12 @@ const App: FC = () => {
 
 	return (
 		<div className="App">
-			{isLoading ? <div>Loading...</div> : <AppRouter />}
+			{isLoading || !cookies.isAuthenticated ? (
+				// TODO: Add a splash screen
+				<div>Loading...</div>
+			) : (
+				<AppRouter />
+			)}
 		</div>
 	);
 };
