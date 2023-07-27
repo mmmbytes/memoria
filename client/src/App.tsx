51ae85fa010d7c5d33e2f70619522c45860ff2a0
Mia <mia001.tapia@gmail.com>
2023-07-27 14:50:48 -0700

    Test 11

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,14 @@
 import './App.css';
 
-import { FC, useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
+import { FC } from 'react';
 
 import AppRouter from './AppRouter';
 
 const App: FC = () => {
-	const [cookies] = useCookies(['isAuthenticated']);
-	const [isLoading, setLoading] = useState<boolean>(true);
-
-	console.log('App: isAuthenticated: ', cookies.isAuthenticated);
-	useEffect(() => {
-		if (!cookies.isAuthenticated) {
-			window.location.href = process.env.REACT_APP_LOGIN_URL;
-		}
-		setLoading(false);
-	}, [cookies.isAuthenticated]);
-
+	console.log('App');
 	return (
 		<div className="App">
-			{isLoading || !cookies.isAuthenticated ? (
-				// TODO: Add a splash screen
-				<div>Loading...</div>
-			) : (
-				<AppRouter />
-			)}
+			<AppRouter />
 		</div>
 	);
 };
--- client/src/views/NoteWorkspace.tsx
+++ client/src/views/NoteWorkspace.tsx
@@ -1,10 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 import NoteManager from '../components/NoteManager';
 
 const NoteWorkspace: FC = () => {
 	console.log('NoteWorkspace');
-	return <NoteManager />;
+
+	const [cookies] = useCookies(['isAuthenticated']);
+	const [isLoading, setLoading] = useState<boolean>(true);
+
+	console.log('NoteWorkspace1: isAuthenticated: ', cookies.isAuthenticated);
+	useEffect(() => {
+		if (!cookies.isAuthenticated) {
+			window.location.href = process.env.REACT_APP_LOGIN_URL;
+		}
+		setLoading(false);
+	}, [cookies.isAuthenticated]);
+
+	return isLoading || !cookies.isAuthenticated ? (
+		<div>Loading...</div>
+	) : (
+		<NoteManager />
+	);
 };
 
 export default NoteWorkspace;
