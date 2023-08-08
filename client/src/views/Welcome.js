4f1a80ed1ed08d001eb8b61797782f5d6d6a65e9
Mia <mia001.tapia@gmail.com>
2023-08-07 17:18:53 -0700

    Introduce Welcome page for new and unsigned users.

--- client/src/views/Welcome.js
+++ client/src/views/Welcome.js
@@ -1,7 +1,23 @@
+import './Welcome.css';
+
+import { SignInButton } from '../components/Buttons';
+import MemoriaLogo from '../images/memoria-logo-large.png';
+
 function Welcome() {
 	return (
-		<div>
-			<h1>MEMORIA: Your thoughts. Ungtangled.</h1>
+		<div className="welcome">
+			<div className="welcome__heading">
+				<h1 className="welcome__header">MEMORIA</h1>
+				<div className="welcome__logo">
+					<img
+						className="welcome__logo--img"
+						src={MemoriaLogo}
+						alt="Memoria Logo"
+					/>
+					<h2 className="welcome__logo--txt">Your thoughts. Untangled.</h2>
+				</div>
+			</div>
+			<SignInButton className="welcome__btn" />
 		</div>
 	);
 }
