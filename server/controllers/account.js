8ff02d956ad8ab3f7dc11022a3a24d896b04cfa5
Mia <mia001.tapia@gmail.com>
2023-11-07 13:18:07 -0800

    Implement service layers for account management and fetching account details.

--- server/controllers/account.js
+++ server/controllers/account.js
@@ -1,19 +1,21 @@
-const jwt = require('jsonwebtoken');
-
 const accountService = require('../services/account');
 
 const deleteAccount = async (req, res) => {
-	const sub = req.sub;
-	const idToken = req.idToken;
-	const decoded = jwt.decode(idToken);
-	const username = decoded['cognito:username'] || decoded['username'];
-
 	try {
-		await accountService.deleteAccount(sub, username);
+		await accountService.deleteAccount(req.sub, req.username);
 		res.status(200).json({ message: 'Account and notes successfully deleted' });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 };
 
-module.exports = { deleteAccount };
+const getUserDetails = async (req, res) => {
+	try {
+		const userDetails = await accountService.getUserDetails(req.username);
+		res.status(200).json(userDetails);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+module.exports = { deleteAccount, getUserDetails };
--- server/services/account.js
+++ server/services/account.js
@@ -1,19 +1,47 @@
 const {
 	CognitoIdentityProviderClient,
 	AdminDeleteUserCommand,
+	AdminGetUserCommand,
 } = require('@aws-sdk/client-cognito-identity-provider');
 
 const noteService = require('./note');
 const REGION = 'us-east-1';
 const cognitoClient = new CognitoIdentityProviderClient({ region: REGION });
 
+const getCognitoParams = (username) => ({
+	UserPoolId: process.env.USER_POOL_ID,
+	Username: username,
+});
+
 const deleteAccount = async (sub, username) => {
+	const params = getCognitoParams(username);
 	await noteService.deleteAllNotes(sub);
-	const deleteCommand = new AdminDeleteUserCommand({
-		UserPoolId: process.env.USER_POOL_ID,
-		Username: username,
-	});
+	const deleteCommand = new AdminDeleteUserCommand(params);
 	await cognitoClient.send(deleteCommand);
 };
 
-module.exports = { deleteAccount };
+const getUserDetails = async (username) => {
+	const params = getCognitoParams(username);
+
+	try {
+		const response = await cognitoClient.send(new AdminGetUserCommand(params));
+
+		const nameDetails = response.UserAttributes.find(
+			(attr) => attr.Name === 'name'
+		);
+		const emailDetails = response.UserAttributes.find(
+			(attr) => attr.Name === 'email'
+		);
+
+		const userDetails = {
+			name: nameDetails ? nameDetails.Value : null,
+			email: emailDetails ? emailDetails.Value : null,
+		};
+		return userDetails;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+};
+
+module.exports = { deleteAccount, getUserDetails };
