fe481612d01432c0541acd7c5e712e62ced3198d
Mia <mia001.tapia@gmail.com>
2023-11-21 14:09:38 -0800

    Update 12

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-	if (process.env.NODE_ENV !== 'UAT') {
+	if (process.env.NODE_ENV === 'development') {
 		app.use(
 			'/api',
 			createProxyMiddleware({
