456a7f33e8a09651dcdcf79d36f2094ed0be7a87
Mia <mia001.tapia@gmail.com>
2024-01-30 12:31:23 -0800

    Refine data passed to testInsights function for clarity and efficiency.

--- main_application/server/controllers/noteInsights.js
+++ main_application/server/controllers/noteInsights.js
@@ -1,10 +1,11 @@
 const insightsService = require('../services/noteInsights');
 
 const testInsights = async (req, res) => {
+	const { message } = req.body;
 	try {
 		console.log('testInsights called');
-		console.log(req.body);
-		const testMessage = await insightsService.testInsights(req.body);
+		console.log(message);
+		const testMessage = await insightsService.testInsights(message);
 		res.status(200).json(testMessage);
 	} catch (error) {
 		console.error(error);
