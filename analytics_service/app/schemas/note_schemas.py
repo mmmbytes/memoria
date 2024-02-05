1de595062362a1fd6e69d9e4709a3b3dd9af8a87
Mia <mia001.tapia@gmail.com>
2024-02-05 15:23:52 -0800

    Refactor note_schemas to accurately represent MongoDB ObjectId.

--- analytics_service/app/schemas/note_schemas.py
+++ analytics_service/app/schemas/note_schemas.py
@@ -1,11 +1,20 @@
 from pydantic import BaseModel, Field
 from typing import List
+from bson import ObjectId
 
-class NoteID(BaseModel):
-    oid: str = Field(..., alias='$oid')
+class PyObjectID(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError(f'Invalid ObjectId: {v}')
+        return str(v)
 
 class Note(BaseModel):
-    id: NoteID
+    _id: PyObjectID = Field(..., alias='id')
     textbody: str
 
 class NotesList(BaseModel):
