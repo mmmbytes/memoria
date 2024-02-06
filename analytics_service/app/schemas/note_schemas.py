10cdd3813b8e8f9e909d47eb74e173d5edd40171
Mia <mia001.tapia@gmail.com>
2024-02-05 18:37:47 -0800

    Update note schemas.

--- analytics_service/app/schemas/note_schemas.py
+++ analytics_service/app/schemas/note_schemas.py
@@ -1,20 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import List
-from bson.objectid import ObjectId
-
-class PyObjectID(str):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError(f'Invalid ObjectId: {v}')
-        return str(v)
+from pydantic-mongo import ObjectIdField
 
 class Note(BaseModel):
-    id: PyObjectID = Field(..., alias='_id')
+    id: ObjectIdField = Field(alias="_id")
     textbody: str
 
 class NotesList(BaseModel):
