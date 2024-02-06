0a99068fd89407abe799444cb03d9c97cbabe4fb
Mia <mia001.tapia@gmail.com>
2024-02-05 17:27:52 -0800

    Add more detailed validation error handling.

--- analytics_service/app/app.py
+++ analytics_service/app/app.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel, ValidationError
 from app.services.text_embedding import get_text_embedding
 from app.schemas.note_schemas import NotesList
 
 app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={"message": "Validation Error", "details": exc.errors()},
+    )
 
 @app.post("/notes_similarity")
 async def notes_similarity(data: NotesList):
