a0f5ae1323855106fe2d50387c5b3d98132ad0ed
Mia <mia001.tapia@gmail.com>
2024-02-08 14:02:10 -0800

    Test 1.

--- analytics_service/app/app.py
+++ analytics_service/app/app.py
@@ -1,14 +1,14 @@
+from typing import List
 from fastapi import FastAPI
 
-from app.schemas.note_schemas import NotesList
+from app.schemas.note_schemas import Note
 from app.services.notes_similarity_analyzer import find_notes_similarity
 
 app = FastAPI()
 
 @app.post("/notes_similarity")
-async def notes_similarity(data: NotesList):
+async def notes_similarity(data: List[Note]):
     print(data)
-    print(data.notes)
-    similarities = find_notes_similarity(data.notes)
+    similarities = find_notes_similarity(data)
     print(similarities)
     return {"message": "Data received successfully!"}
--- analytics_service/app/schemas/note_schemas.py
+++ analytics_service/app/schemas/note_schemas.py
@@ -1,11 +1,7 @@
-from typing import List
-
 from pydantic import BaseModel
 
 class Note(BaseModel):
     id: str 
     textbody: str
 
-class NotesList(BaseModel):
-    notes: List[Note]   
   
