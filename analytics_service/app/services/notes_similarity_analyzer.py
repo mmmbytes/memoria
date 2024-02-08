3443f190c1003c6fe2a1ded4b06d200b19e9308e
Mia <mia001.tapia@gmail.com>
2024-02-08 14:50:39 -0800

    Test 2.

--- analytics_service/app/services/notes_similarity_analyzer.py
+++ analytics_service/app/services/notes_similarity_analyzer.py
@@ -5,13 +5,15 @@ from itertools import combinations
 from .text_embedding import get_text_embedding
 from app.schemas.note_schemas import Note
 
+EMBED_TEXT_MODEL = "amazon.titan-embed-text-v1"
+
 def cosine_similarity(vecA, vecB):
   dot_product = np.dot(vecA, vecB)
   magnitude = np.linalg.norm(vecA) * np.linalg.norm(vecB)
   return dot_product / magnitude
 
 def find_notes_similarity(notes: List[Note]):
-  embeddings = [(note.id, get_text_embedding(note.textbody)) for note in notes]
+  embeddings = [(note.id, get_text_embedding(note.textbody, EMBED_TEXT_MODEL)) for note in notes]
   similarities = [
     {
       "source": source_id,
