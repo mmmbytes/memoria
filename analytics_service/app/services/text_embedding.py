dc1997558e7f4cd19ed32a0f0f199f6cdfb52018
Mia <mia001.tapia@gmail.com>
2024-02-08 20:54:32 -0800

    Add more robust error handling for text_embedding functions.

--- analytics_service/app/services/text_embedding.py
+++ analytics_service/app/services/text_embedding.py
@@ -1,40 +1,44 @@
+import logging
+
 import boto3
 import json
 from botocore.config import Config
 from botocore.exceptions import BotoCoreError, ClientError
 import numpy as np
 
-AWS_REGION = 'us-east-1'
+AWS_REGION = "us-east-1"
+
 
 def create_bedrock_client():
     config = Config(
-        region_name = AWS_REGION,
-        signature_version = 'v4',
-        retries = {
-            'max_attempts': 10,
-            'mode': 'standard'
-        }
+        region_name=AWS_REGION,
+        signature_version="v4",
+        retries={"max_attempts": 10, "mode": "standard"},
     )
-    bedrock_client = boto3.client(service_name='bedrock-runtime', config=config)
-    return bedrock_client
+    try:
+        bedrock_client = boto3.client(service_name="bedrock-runtime", config=config)
+        return bedrock_client
+    except (BotoCoreError, ClientError) as e:
+        logging.error(f"Error creating Bedrock client: {e}")
+        raise
+    except Exception as e:
+        logging.error(f"Unkown error while creating Bedrock client: {e}")
+        raise
+
 
 def get_text_embedding(inputText, modelId):
     client = create_bedrock_client()
-    body = json.dumps({
-        "inputText": inputText
-    })
+    body = json.dumps({"inputText": inputText})
     try:
-        response = client.invoke_model(body=body, modelId=modelId, accept='*/*', contentType='application/json')
-        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-            print(f"Error: Received non-200 response status code: {response['ResponseMetadata']['HTTPStatusCode']}")
-            return None
-        try:
-            response_body = json.loads(response.get('body').read())
-            embedding = response_body['embedding']
-            return np.array(embedding)
-        except json.JSONDecodeError as e:
-            print(f"Error parsing response JSON: {e}")
-            return None
-    except (BotoCoreError, ClientError) as e:
-        print(f"Error calling AWS Bedrock service: {e}")
-        return None
+        response = client.invoke_model(
+            body=body, modelId=modelId, accept="*/*", contentType="application/json"
+        )
+        response_body = json.loads(response.get("body").read())
+        embedding = response_body["embedding"]
+        return np.array(embedding)
+    except ClientError as e:
+        logging.error(f"Error invoking Bedrock model: {e}")
+        raise
+    except Exception as e:
+        logging.error(f"Unknown error while invoking Bedrock model: {e}")
+        raise
