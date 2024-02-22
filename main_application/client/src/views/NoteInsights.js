e95fd8b25677ecbdb7de7fbbfcb793ba61660eec
Mia <mia001.tapia@gmail.com>
2024-02-22 15:10:21 -0800

    Introduce conditional rendering for NoteInsights to toggle between Intro and Network views.

--- main_application/client/src/views/NoteInsights.js
+++ main_application/client/src/views/NoteInsights.js
@@ -1,45 +1,30 @@
-import './styles/NoteInsights.css';
-
 import { useState } from 'react';
 
-import { NoteInsightsButton } from '../components/buttons/NoteInsightsButtons';
-import { NetworkVisualization } from '../components/NetworkVisualization';
 import { getNoteInsights } from '../services/NoteInsights';
-import btnMod from '../sharedStyles/button.module.css';
+import NoteInsightsIntro from './NoteInsightsIntro';
+import NoteInsightsNetwork from './NoteInsightsNetwork';
 
 function NoteInsights() {
+	const [view, setView] = useState('intro');
 	const [notesData, setNotesData] = useState(null);
 
 	async function visualizeNotesData() {
 		try {
 			const fetchedData = await getNoteInsights();
 			setNotesData(fetchedData);
+			setView('network');
 		} catch (error) {
 			console.error(error);
 		}
 	}
 
 	return (
-		<div className="note-insights">
-			<div className="note-insights__heading">
-				<h1 className="note-insights__title">
-					{'Discover the hidden networks within your thoughts.'}
-				</h1>
-				<div className="note-insights__btn">
-					<NoteInsightsButton
-						className={`${btnMod.btn} ${btnMod.btnStyled} ${btnMod.btnMisc} note-insights__btn`}
-						onClick={visualizeNotesData}
-					/>
-				</div>
-			</div>
-			<div className="note-insights__network">
-				{notesData && (
-					<NetworkVisualization
-						nodes={notesData.notesList}
-						links={notesData.similarityData}
-					/>
-				)}
-			</div>
+		<div>
+			{view === 'intro' ? (
+				<NoteInsightsIntro visualizeNotesData={visualizeNotesData} />
+			) : (
+				<NoteInsightsNetwork notesData={notesData} />
+			)}
 		</div>
 	);
 }
