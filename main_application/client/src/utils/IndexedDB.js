5c7a38a451212ed230ae71cabd2d347b94ecb29e
Mia <mia001.tapia@gmail.com>
2024-02-26 13:41:09 -0800

    Add more console.log statement for debugging fetchIndexedDB.

--- main_application/client/src/utils/IndexedDB.js
+++ main_application/client/src/utils/IndexedDB.js
@@ -40,12 +40,18 @@ export async function cacheIndexedDB(databaseName, objectStoreName, notesData) {
 }
 
 export async function fetchIndexedDB(databaseName, objectStoreName) {
+	console.log('fetchIndexedDB: ', databaseName, objectStoreName);
 	try {
 		let db = await initIndexedDB({ databaseName, objectStoreName });
+		console.log('fetchIndexedDB: ', db);
 		let transaction = db.transaction(objectStoreName, 'readonly');
+		console.log('fetchIndexedDB: ', transaction);
+
 		let objectStore = transaction.objectStore(objectStoreName);
+		console.log('fetchIndexedDB: ', objectStore);
 		let data = await objectStore.getAll();
-		return data[0];
+		console.log('fetchData:', data);
+		return data;
 	} catch (error) {
 		console.error('Error retrieving data: ', error);
 	}
