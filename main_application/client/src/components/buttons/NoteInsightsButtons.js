cee7ecb6744206e02f618e759692c70581074a24
Mia <mia001.tapia@gmail.com>
2024-03-02 16:17:46 -0800

    Add a loading animation to Reveal Insights button.

--- main_application/client/src/components/buttons/NoteInsightsButtons.js
+++ main_application/client/src/components/buttons/NoteInsightsButtons.js
@@ -1,7 +1,24 @@
-export function NoteInsightsButton({ className, onClick }) {
+import { IoIosArrowDown } from 'react-icons/io';
+
+export function RevealInsightsButton({ className, onClick, loading }) {
 	return (
-		<button type="button" className={className} onClick={onClick}>
-			Reveal Insights
+		<button
+			type="button"
+			className={className}
+			onClick={onClick}
+			loading={loading}
+		>
+			{loading ? 'Loading Insights' : 'Reveal Insights'}
 		</button>
+	);
+}
+
+export function CloseDetailsButton({ className, onClick }) {
+	return (
+		<IoIosArrowDown
+			type="button"
+			className={className}
+			onClick={onClick}
+		></IoIosArrowDown>
 	);
 }
