00808824ec702df87b6339a840cdc16d026f1fa6
Mia <mia001.tapia@gmail.com>
2024-03-07 13:33:06 -0800

    Refactor text_embedding, and note_similarity_analyzer to adhere to pep8 standards. Add custom exception for text embedding generation errors.

--- analytics_service/app/services/note_similarity_analyzer.py
+++ analytics_service/app/services/note_similarity_analyzer.py
@@ -1,48 +1,29 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import List
+from typing import Callable, Dict, List, Union
 import numpy as np
 from itertools import combinations
 
-from .text_embedding import get_text_embedding
+from .text_embedding import generate_batch_note_embeddings
 from app.schemas.note_schemas import Note
 
-_EMBEDDING_TEXT_MODEL = "amazon.titan-embed-text-v1"
 
-
-def cosine_similarity(vecA, vecB):
-    dot_product = np.dot(vecA, vecB)
-    magnitude = np.linalg.norm(vecA) * np.linalg.norm(vecB)
+def cosine_similarity(vec_a: np.ndarray, vec_b: np.ndarray) -> float:
+    """Calculate the cosine similarity between two vectors."""
+    dot_product = np.dot(vec_a, vec_b)
+    magnitude = np.linalg.norm(vec_a) * np.linalg.norm(vec_b)
     return dot_product / magnitude
 
 
-def generate_batch_note_embeddings(notes: List[Note]):
-    with ThreadPoolExecutor() as executor:
-        note_embedding_futures = {
-            executor.submit(
-                get_text_embedding, note.textbody, _EMBEDDING_TEXT_MODEL
-            ): note
-            for note in notes
-        }
-        note_embeddings_list = []
-        for future in as_completed(note_embedding_futures):
-            note = note_embedding_futures[future]
-            try:
-                embedding = future.result()
-                note_embeddings_list.append((note.id, embedding))
-            except Exception as exc:
-                raise Exception(
-                    f"Note {note.id} raised an exception while attempting to generate text embedding: {exc}"
-                )
-    return note_embeddings_list
-
-
-def find_note_similarities(notes: List[Note]):
+def find_note_similarities(
+    notes: List[Note],
+    similarity_func: Callable[[np.ndarray, np.ndarray], float] = cosine_similarity,
+) -> List[Dict[str, Union[str, float]]]:
+     """Compute semantic similarities between notes using a specified similarity function to compare text embeddings."""
     note_embeddings_list = generate_batch_note_embeddings(notes)
     note_similarities = [
         {
             "source": source_note_id,
             "target": target_note_id,
-            "value": cosine_similarity(source_embedding, target_embedding),
+            "value": similarity_func(source_embedding, target_embedding),
         }
         for (i, (source_note_id, source_embedding)), (
             j,
